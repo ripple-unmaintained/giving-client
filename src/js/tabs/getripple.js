dc07b20c3a2b3eec0b662a19e65e77faa4140729
Justin Hernandez <justin@ripple.com>
2013-05-02 17:06:29 -0700

    updating state logic
    Signed-off-by: Justin Hernandez <justin@ripple.com>

--- src/js/tabs/getripple.js
+++ src/js/tabs/getripple.js
@@ -1,10 +1,11 @@
 var util = require('util'),
-    webutil = require('../util/web'),
-    Tab = require('../client/tab').Tab,
-    Amount = ripple.Amount;
+  webutil = require('../util/web'),
+  Tab = require('../client/tab').Tab,
+  Amount = ripple.Amount,
+  giveawayTotal = 4,
+  giveawayFunding = 500;
 
-var GetRippleTab = function ()
-{
+var GetRippleTab = function() {
   Tab.call(this);
 
   //this.on('retrigger', this.handleRetrigger.bind(this));
@@ -14,59 +15,95 @@ util.inherits(GetRippleTab, Tab);
 
 GetRippleTab.prototype.mainMenu = 'getripple';
 
-GetRippleTab.prototype.generateHtml = function ()
-{
+GetRippleTab.prototype.generateHtml = function() {
   return require('../../jade/tabs/getripple.jade')();
 };
 
 
-GetRippleTab.prototype.angular = function (module)
-{
+GetRippleTab.prototype.angular = function(module) {
   var self = this,
-      app = this.app;
+    app = this.app;
 
   module.controller('GetRippleCtrl', ['$scope', '$routeParams', '$rootScope', 'rpId',
-                                 function ($scope, $routeParams, $rootScope, $id)
-  {
+
+  function($scope, $routeParams, $rootScope, $id) {
+
+    var stateUpdated = false;
+    $scope.register = false;
+    $scope.payout = giveawayFunding;
+
+    // watch user blob for changes
+    $rootScope.$watch('userBlob', function() {
+      // if giveaway register is set
+      if ($rootScope.userBlob.data.giveaway_register) {
+        $scope.register = $rootScope.userBlob.data.giveaway_register;
+        // if state hasn't been updated
+        if (!stateUpdated) {
+          var total = 0;
+          // fetch state determined by user id
+          $.get(Options.giveawayServer + '/user/state/' + $scope.register.id, function(user) {
+
+            // if user has already been funded hide page
+            if (user.funded)
+              $scope.funded = true;
+
+            // iterate states
+            _.each(user.state, function(v, i) {
+              //  state is true then update class
+              if (v) $scope.updateState(i);
+              total += (v) ? 1 : 0;
+            });
+
+            // if total is giveaway total then show finish
+            if (total == giveawayTotal) $scope.finish = true;
+            // don't run again
+            stateUpdated = true;
+          });
+        }
+      }
+    });
+
+
     if (!$id.loginStatus) return $id.goId(); //Don't see this page until you log in.
 
-    $scope.clickCode = function() {
-      $rootScope.codeClass='done';
-      areWeDoneYet();
-    }
-    $scope.clickWiki = function() {
-      $rootScope.wikiClass='done';
-      areWeDoneYet();
-    }
-    $scope.clickVideo = function() {
-      $rootScope.videoClass='done';
-      areWeDoneYet();
-    }
-    $scope.clickList = function() {
-      $rootScope.listClass='done';
-      areWeDoneYet();
-    }
+    // monitor click state
+    $scope.clickState = function(item) {
+      // return false if already done
+      if ($rootScope[item + 'Class'] == 'done') return false;
 
-    function areWeDoneYet() {
-      $scope.finish = (
-        !($rootScope.claim) &&
-        $rootScope.codeClass=='done' && 
-        $rootScope.wikiClass=='done' && 
-        $rootScope.videoClass=='done' &&
-        $rootScope.listClass=='done');
-    }
-    areWeDoneYet();
+      $.post(Options.giveawayServer + '/user/' + $scope.register.id, {
+        action: 'state',
+        state: item,
+        register: $scope.register.hash
+      }, function(data) {
+        $scope.updateState(item);
+        // if state has been all clicked then show claim
+        if (data.count == 4) {
+          $scope.finish = true;
+          $scope.$apply();
+        }
+      });
+    };
 
+    // update state locally
+    $scope.updateState = function(item) {
+      // return false if already done
+      if ($rootScope[item + 'Class'] == 'done') return false;
 
-    $scope.congrats = function(){
-      $rootScope.claim = true;
-      areWeDoneYet();
+      $rootScope[item + 'Class'] = 'done';
+      $rootScope.$apply();
+    };
+
+    // do funding
+    $scope.congrats = function() {
+      $scope.finish = false;
+      $scope.claim = true;
     };
 
   }]);
 };
 
-GetRippleTab.prototype.handleRetrigger = function () {
+GetRippleTab.prototype.handleRetrigger = function() {
   var $scope = $('#t-send').data('$scope');
   if ($scope && $scope.mode !== 'form') {
     $scope.reset();
